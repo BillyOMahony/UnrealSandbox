a5c888c9c54de7529987c97f2d03fed5faa1d15f
Billy O'Mahony <omahonybilly@gmail.com>
2018-06-22 12:07:04 +0100

    Restructured remaining classes

--- Source/UnrealSandbox/UnrealSandboxGameMode.cpp
+++ Source/UnrealSandbox/UnrealSandboxGameMode.cpp
@@ -1,8 +1,8 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 #include "UnrealSandboxGameMode.h"
-#include "UnrealSandboxHUD.h"
-#include "UnrealSandboxCharacter.h"
+#include "FirstPersonCharacter/UnrealSandboxHUD.h"
+#include "FirstPersonCharacter/UnrealSandboxCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AUnrealSandboxGameMode::AUnrealSandboxGameMode()
